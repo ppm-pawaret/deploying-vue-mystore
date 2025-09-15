ea072965f3030e5ad27909c7a0b1f8b5800c8b3b
pawaret <ppm.pawaret@gmail.com>
2025-09-15 16:20:45 +0700

    update product api

--- api/products.js
+++ api/products.js
@@ -1,58 +1,51 @@
 [
   {
-    id: 1,
-    name: "Bounty Hunting System",
-    description:
-      "อยากตามล่าใครสักคนไหม? ระบบนี้ให้คุณตั้งค่าหัวผู้เล่นคนอื่น แล้วปล่อยให้เหล่านักล่าออกไล่ล่าเพื่อชิงรางวัล!",
-    price: 1000,
-    images: ["bounty_system"],
+    "id": 1,
+    "name": "Bounty Hunting System",
+    "description": "อยากตามล่าใครสักคนไหม? ระบบนี้ให้คุณตั้งค่าหัวผู้เล่นคนอื่น แล้วปล่อยให้เหล่านักล่าออกไล่ล่าเพื่อชิงรางวัล!",
+    "price": 1000,
+    "images": ["bounty_system"]
   },
   {
-    id: 2,
-    name: "Identity System",
-    description:
-      "ระบบจัดการตัวตนที่สมบูรณ์แบบ ทั้งการสมัครสมาชิก, การเข้าสู่ระบบ และการจัดการข้อมูลผู้ใช้งาน ช่วยให้การยืนยันตัวตนเป็นเรื่องง่ายและปลอดภัยสำหรับธุรกิจของคุณ",
-    price: 1500,
-    images: ["identity_register"],
+    "id": 2,
+    "name": "Identity System",
+    "description": "ระบบจัดการตัวตนที่สมบูรณ์แบบ ทั้งการสมัครสมาชิก, การเข้าสู่ระบบ และการจัดการข้อมูลผู้ใช้งาน ช่วยให้การยืนยันตัวตนเป็นเรื่องง่ายและปลอดภัยสำหรับธุรกิจของคุณ",
+    "price": 1500,
+    "images": ["identity_register"]
   },
   {
-    id: 3,
-    name: "Job System",
-    description:
-      "ระบบจัดการงานที่ช่วยให้การทำงานมีประสิทธิภาพมากขึ้น ตั้งแต่การมอบหมายงาน, ติดตามความคืบหน้า ไปจนถึงการจัดทำรายงาน ช่วยให้คุณไม่พลาดทุกรายละเอียดของงานสำคัญ",
-    price: 2000,
-    images: ["working"],
+    "id": 3,
+    "name": "Job System",
+    "description": "ระบบจัดการงานที่ช่วยให้การทำงานมีประสิทธิภาพมากขึ้น ตั้งแต่การมอบหมายงาน, ติดตามความคืบหน้า ไปจนถึงการจัดทำรายงาน ช่วยให้คุณไม่พลาดทุกรายละเอียดของงานสำคัญ",
+    "price": 2000,
+    "images": ["working"]
   },
   {
-    id: 4,
-    name: "School Selected System",
-    description:
-      "ระบบจัดการรายชื่อนักเรียนที่ผ่านการคัดเลือกเข้าเรียน ช่วยให้คุณนำเข้าและจัดการข้อมูลได้อย่างง่ายดาย พร้อมทั้งสามารถประกาศผลการคัดเลือกได้อย่างรวดเร็วและเป็นระบบ",
-    price: 1200,
-    images: ["schoolselected"],
+    "id": 4,
+    "name": "School Selected System",
+    "description": "ระบบจัดการรายชื่อนักเรียนที่ผ่านการคัดเลือกเข้าเรียน ช่วยให้คุณนำเข้าและจัดการข้อมูลได้อย่างง่ายดาย พร้อมทั้งสามารถประกาศผลการคัดเลือกได้อย่างรวดเร็วและเป็นระบบ",
+    "price": 1200,
+    "images": ["schoolselected"]
   },
   {
-    id: 5,
-    name: "Skin System",
-    description:
-      "ระบบจัดการชุดและรูปลักษณ์ของตัวละครที่พัฒนาบนพื้นฐาน esx_skin ช่วยให้ผู้เล่นสามารถปรับแต่งตัวละครได้อย่างอิสระและบันทึกสกินที่ชื่นชอบได้ มาพร้อมอินเทอร์เฟซที่ใช้งานง่ายและประสิทธิภาพสูง",
-    price: 1800,
-    images: ["skin"],
+    "id": 5,
+    "name": "Skin System",
+    "description": "ระบบจัดการชุดและรูปลักษณ์ของตัวละครที่พัฒนาบนพื้นฐาน esx_skin ช่วยให้ผู้เล่นสามารถปรับแต่งตัวละครได้อย่างอิสระและบันทึกสกินที่ชื่นชอบได้ มาพร้อมอินเทอร์เฟซที่ใช้งานง่ายและประสิทธิภาพสูง",
+    "price": 1800,
+    "images": ["skin"]
   },
   {
-    id: 6,
-    name: "Casino System",
-    description:
-      "ระบบคาสิโนที่ครบวงจร ให้ผู้เล่นได้เสี่ยงโชคกับเกมต่าง ๆ ทั้งสล็อตแมชชีนและโป๊กเกอร์",
-    price: 2500,
-    images: [],
+    "id": 6,
+    "name": "Casino System",
+    "description": "ระบบคาสิโนที่ครบวงจร ให้ผู้เล่นได้เสี่ยงโชคกับเกมต่าง ๆ ทั้งสล็อตแมชชีนและโป๊กเกอร์",
+    "price": 2500,
+    "images": []
   },
   {
-    id: 7,
-    name: "Example System",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam debitis consequatur suscipit. Assumenda suscipit sint nobis, facere commodi quia beatae minus mollitia, libero quidem quos atque! Fugiat dolor repudiandae laudantium. Impedit soluta numquam unde, voluptatibus quos sequi id ipsum eaque, corrupti ex adipisci. Totam quod placeat tenetur cum provident sint.",
-    price: 9999,
-    images: [],
-  },
+    "id": 7,
+    "name": "Example System",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam debitis consequatur suscipit. Assumenda suscipit sint nobis, facere commodi quia beatae minus mollitia, libero quidem quos atque! Fugiat dolor repudiandae laudantium. Impedit soluta numquam unde, voluptatibus quos sequi id ipsum eaque, corrupti ex adipisci. Totam quod placeat tenetur cum provident sint.",
+    "price": 9999,
+    "images": []
+  }
 ]
